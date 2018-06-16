e5b5bde2be15b8884e1c561e73db4e1f253b1712
Dimitry Karalov <karalov.d@gmail.com>
2018-06-16 19:36:15 -0400

    updated final

--- block_sites.py
+++ block_sites.py
@@ -1,40 +1,43 @@
+#!/usr/bin/python3
 import time
 from datetime import datetime
 #hosts_path=r'C:\Windows\System32\drivers\etc\hosts'
-hosts_path=r'C:\Games\hosts.txt'
+#hosts_path=r'C:\Games\hosts.txt'
+hosts_path='hosts'
 redirect='127.0.0.1'
 website_list=["www.facebook.com","facebook.com","ok.ru","www.ok.ru","vk.com","www.vk.com"]
-start_block_hour=10
-end_block_hour=17
-
-def remove_line(record):
-    new_content=[]
-    with open(hosts_path,"r") as fr:
-        for line in fr.readlines():
-            if line != record:
-                new_content.append(line)
-    with open(hosts_path,"w") as fw:
-        fw.writelines(new_content)
+start_block_hour=25
+end_block_hour=28
 
 def add_block():
-    print("Adding block")
-    with open(hosts_path,"a") as f:
+    with open(hosts_path,"r+") as f:
+        content = f.read()
         for site in website_list:
-            f.write(redirect + " " + site + "\n")
+            if site not in content:
+                  print("Adding ",site)
+                  f.write(redirect + " " + site + "\n")
 
 def remove_block():
-    print("Removing block")
-    for site in website_list:
-       remove_line(redirect + " " + site + "\n")
+    with open(hosts_path,"r+") as f:
+        content=f.readlines()
+        removed=False
+        for site in website_list:
+            if any(site in line for line in content):
+                   print("Removing",site)
+                   removed=True
+                   content.remove(redirect + " " + site + "\n")
+        if removed :
+           f.seek(0)
+           f.writelines(content)
+           f.truncate()
 
 while 1:
-    now_hour=datetime.now().hour
+    now_hour=datetime.now().minute
+    print("Now hour: %s. SItes will be locked at %s, unlocked at %s" % (now_hour,start_block_hour,end_block_hour))
     if  ( now_hour >= start_block_hour ) and ( now_hour < end_block_hour):
+        print("working time")
         add_block()
-        while now_hour < end_block_hour:
-            now_hour=datetime.now().hour
-            print("Now hour: %s. Waiting for %s to stop blocking" % (now_hour,end_block_hour))
-            time.sleep(60)
+    else:
+        print("fun time")
         remove_block()
-    print("Now hour: %s. Waiting for %s to start blocking" % (now_hour,start_block_hour))
     time.sleep(60)
